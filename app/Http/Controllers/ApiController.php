f9b794d1484a89d88003fc5b5787fa87dfcf3312
edgeq <edgarq86@gmail.com>
2019-12-18 12:43:30 -0600

    Succesfully manipulated API to spec

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,12 +16,45 @@ class ApiController extends Controller
     public function get_user()
     {
         $client = new Client();
-        $request = new Request('GET', 'https://randomuser.me/api', ['headers' => ['Content-Type' => 'application/json']]);
+        $request = new Request('GET', 'https://randomuser.me/api');
         $promise = $client->sendAsync($request)
             ->then(
                 function (Response $resp) {
                     $body = $resp->getBody()->getContents();
-                    return json_decode($body, true);
+                    /**
+                     * Manipulates API to include the following:
+                     *
+                     *
+                     * { name: [Title => results.name.title] [first => results.name.first] [last => results.name.last]}
+                     *   email: [email = results.email]
+                     *   photo: [photo url => results.picture.thumbnail]
+                     *   birthday: [formatted birthday, April 24, 2012 => redults.dob(formatted)]
+                     * }
+                     */
+                    // $customBody = $body[0]->name;
+                    // echo $customBody;
+                    $response   = json_decode($body, true);
+                    $title      = $response["results"][0]["name"]["title"];
+                    $first_name = $response["results"][0]["name"]["first"];
+                    $last_name  = $response["results"][0]["name"]["last"];
+                    $email      = $response["results"][0]["email"];
+                    $photo      = $response["results"][0]["picture"]["thumbnail"];
+                    $birthday   = $response["results"][0]["dob"]["date"];
+                    $dob        = strtotime($birthday);
+                    $dob_formatted = date('F d, Y', $dob);
+                    // return $response["results"][0]["name"]["first"];
+                    $res_object = [
+                        // "title" => $title,
+                        // "first_name" => $first_name,
+                        // "last_name" => $last_name,
+                        "name" => "${title} ${first_name} ${last_name}",
+                        "email" => $email,
+                        "photo" => $photo,
+                        "birthday" => $dob_formatted,
+
+                    ];
+                    return $res_object;
+                    // return json_decode($body, true);
                 },
                 function (RequestException $e) {
                     echo $e->getMessage();
