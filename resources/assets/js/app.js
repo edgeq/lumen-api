fa1508cde2846427c5850e47e51c67fde3680677
edgeq <edgarq86@gmail.com>
2019-12-18 22:27:36 -0600

    front end code mockup rough draft

---
+++ resources/assets/js/app.js
@@ -0,0 +1,1 @@
+alert('wired up');
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,16 +2,35 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Lumen Api</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Lumen Api</title>
 </head>
 
-<body>
+<body class="bg-blue-900">
 
-  <div id="app"></div>
-  <script src="dist/app.js"></script>
+    <div class="container mx-auto h-screen bg-blue-900 flex">
+        <div class="md:flex mx-auto items-center">
+            <div class="bg-gray-100 flex p-6 rounded-lg border">
+                <img src="https://picsum.photos/200" alt="avatar" class="rounded-full" />
+                <div class="flex flex-col items-center justify-center">
+                    <h4>Name goes here</h4>
+                    <div>
+                        email@email.com
+                    </div>
+                    <div>birthday goes here</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script>
+        alert('hello')
+    </script>
 </body>
 
 </html>
